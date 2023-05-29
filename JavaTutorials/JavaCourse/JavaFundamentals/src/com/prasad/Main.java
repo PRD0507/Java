64b869fb9cbc15f1357a949dfcdafc6819316f24
Prasad Desai <prdprasaddesai@gmail.com>
2023-05-29 22:10:04 +0530

    Name added

--- JavaTutorials/JavaCourse/JavaFundamentals/src/com/prasad/Main.java
+++ JavaTutorials/JavaCourse/JavaFundamentals/src/com/prasad/Main.java
@@ -8,10 +8,14 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+
         System.out.println("Enter the Roll No");
         Scanner input = new Scanner(System.in);
         int rollNo = input.nextInt();
         System.out.println("Your Roll No is " + rollNo);
 
+        System.out.println("Enter the Name");
+        String firstName = input.next();
+        System.out.println("First Name " + firstName);
     }
 }
