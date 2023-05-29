402b27b39c15919882e2de3d107a4c3f4e3daeec
Prasad Desai <prdprasaddesai@gmail.com>
2023-05-29 22:30:39 +0530

    Basics

---
+++ JavaTutorials/JavaCourse/Conditions-Loops/ConditionsLoops/src/com/prasad/Main.java
@@ -0,0 +1,13 @@
+package com.prasad;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int salary = 25000;
+        if (salary>=25000){
+            salary += 5000;
+        }
+
+        System.out.println(salary);
+    }
+}
