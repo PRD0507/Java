473964a677e1f77ee332912ff67cbb3480382ae4
Prasad Desai <prdprasaddesai@gmail.com>
2023-05-31 09:00:41 +0530

    Loops and Largest Program

---
+++ JavaTutorials/JavaCourse/JavaFundamentals/src/com/prasad/Loops.java
@@ -0,0 +1,16 @@
+package com.prasad;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the length of the loop");
+        int n = input.nextInt();
+
+        for(int i=0; i<n; i++){
+            System.out.println("i = "+ i);
+        }
+    }
+}
