872e45971400382ee9f36c78ee29625f3a96c865
Prasad Desai <prdprasaddesai@gmail.com>
2023-05-31 09:09:38 +0530

    Reversing a number

---
+++ JavaTutorials/JavaCourse/JavaFundamentals/src/com/prasad/Reverse.java
@@ -0,0 +1,15 @@
+package com.prasad;
+
+public class Reverse {
+    public static void main(String[] args) {
+        int num = 123456;
+
+        int ans = 0;
+        while(num>0){
+            int digit = num % 10;
+            num = num / 10;
+            ans = ans * 10 + digit;
+        }
+        System.out.println("Reverse Number "+ ans);
+    }
+}
